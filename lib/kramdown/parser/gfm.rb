e5b05ac93793a8af38d6adf816707dab1b703fba
Phil Helliwell <phil.helliwell@neon.me.uk>
2020-11-06 15:56:28 +0000

    Respect the 'transliterated_header_ids' option.
    Let kramdown's converters create the IDs rather
    than doing it in the parser.

--- lib/kramdown/parser/gfm.rb
+++ lib/kramdown/parser/gfm.rb
@@ -63,6 +63,9 @@ module Kramdown
             update_text_type(element, child)
           elsif child.type == :html_element
             child
+          elsif child.type == :header && @options[:auto_ids] && @options[:transliterated_header_ids]
+            # Let the kramdown converter create the ID
+            child
           elsif child.type == :header && @options[:auto_ids] && !child.attr.key?('id')
             child.attr['id'] = generate_gfm_header_id(child.options[:raw_text])
             child
